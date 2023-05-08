5eecbae534b5e6a88896ef99ce2f194d32a500af
Prem192002 <premsingh192002@gmail.com>
2023-05-08 16:17:15 +0530

    first commit

---
+++ pract1.java
@@ -0,0 +1,28 @@
+class array{
+
+    void exarray(){
+    int[] age = new int[3];
+
+    age[0]=25;
+    age[1]=20;
+    age[2]=30;
+
+
+    System.out.println(age[0]);
+    System.out.println(age[1]);
+    System.out.println(age[2]);
+    
+    }
+}
+
+public class pract1 {
+
+    public static void main(String[] args) {
+
+        array obj1 = new array();
+
+        obj1.exarray();
+    
+    }
+    
+}
