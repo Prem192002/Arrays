0522ac89b8270f1303f817d54ad5647ce4a8377c
Prem192002 <premsingh192002@gmail.com>
2023-10-11 16:17:52 +0530

    traversing through 2D array

--- practice.java
+++ practice.java
@@ -1,19 +1,21 @@
 class details{
     public void ages(){
-        int[][] ages = new int[2][2];
-        ages[0][0]=15;
-        ages[0][1]=25;
-        ages[1][0]=30;
-        ages[1][1]=45;
+        int[][] ages = {{1,2,3},{4,5,6},{7,8,9}};
+        // ages[0][0]=15;
+        // ages[0][1]=25;
+        // ages[1][0]=30;
+        // ages[1][1]=45;
+        // ages[0][0]=23;
 
-        int f1 = ages[0][0];
-        System.out.println(f1);
-        // System.out.println(ages[1]);
+        for (int i=0;i<2;i++){
+            for (int j=0; j<2;j++){
+                System.out.println(ages[i][j]);
+            }
+            
+        }
         
     } 
 }
-
-
 
 public class practice {
     public static void main(String[] args){
