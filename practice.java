af4beab690c3d59887264c9ef6ec100de8eb03b7
Prem192002 <premsingh192002@gmail.com>
2023-10-11 23:49:04 +0530

    traverse using length

--- practice.java
+++ practice.java
@@ -1,14 +1,14 @@
 class details{
     public void ages(){
-        int[][] ages = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] ages = {{1,2},{4,5},{7,8,9}};
         // ages[0][0]=15;
         // ages[0][1]=25;
         // ages[1][0]=30;
         // ages[1][1]=45;
         // ages[0][0]=23;
 
-        for (int i=0;i<2;i++){
-            for (int j=0; j<2;j++){
+        for (int i=0;i<ages.length;i++){
+            for (int j=0; j<ages[i].length;j++){
                 System.out.println(ages[i][j]);
             }
             
