22b222f6537779d1eb95bed52359b8481bc6e666
Prem192002 <premsingh192002@gmail.com>
2023-10-18 23:54:13 +0530

    missing element

--- arr1.java
+++ arr1.java
@@ -1,23 +1,25 @@
+// You are using Java
 import java.util.*;
-class details{
-    static void fun(int num){
-        Scanner sc = new Scanner(System.in);
-        int[] ages = new int[num];
-        System.out.println("enter the array value:");
-        for (int i=0;i<ages.length;i++){
-        ages[i]=sc.nextInt();
-        }
-    for(int i=0;i<num;i++){
-        System.out.print(ages[i]);
-        }
-    }
-}
-public class arr1{
+class arr1{
     public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the size of the array: ");
-    int num = sc.nextInt();
-        details obj = new details();
-        obj.fun(num);
+        Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            
+            int [] arr = new int[n];
+            
+            for (int i = 0; i<n; i++){
+                arr[i] = sc.nextInt();
+                
+            }
+            
+            int expectedSum = (n+1)*(n+2)/2;
+            int actualSum = 0;
+            
+            for (int num : arr){
+                actualSum += num;
+            }
+            
+            int missingElement = expectedSum - actualSum;
+            System.out.println(missingElement);
+        }
     }
-}
