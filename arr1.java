aa7288c7adbce41573e4e7cb00e94ec148ca44af
Prem192002 <premsingh192002@gmail.com>
2023-10-27 23:56:43 +0530

    access modifier

--- arr1.java
+++ arr1.java
@@ -1,23 +1,38 @@
-// You are using Java
 import java.util.*;
+class student{
+    private int id;
+    private String name;
 
-class arr1{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int x=sc.nextInt();
-        
-        int[] grades = new int[x];
-        for(int i=0;i<x;i++){
-            grades[i] = sc.nextInt();
-            
-        }
-        for(int i=0;i<x;i++){
-            if (grades[i]>=0){
-                System.out.println(grades[i]);
-            }
-            
-        }
-        
+
+    
+
+    public int getID(){
+        return id;
+    }
+    public void setID(int id){
+        this.id=id;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
     }
 }
+
+
+    public class arr1{
+        public static void main(String[] args){
+            Scanner sc= new Scanner(System.in);
+            student obj = new student();
+            System.out.println("enter the id:");
+            obj.setID(sc.nextInt());
+            System.out.println("enter the name:");
+            sc.nextLine();
+            obj.setName(sc.nextLine());
+            System.out.println("the id is:"+obj.getID()+"the name is:"+obj.getName());
+        }
+    }
+
+
