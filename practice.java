10331bc93c8205f4e7511b9c9f0426bbcb13fb53
Prem192002 <premsingh192002@gmail.com>
2024-03-09 21:40:08 +0530

    made the function

--- practice.java
+++ practice.java
@@ -1,17 +1,26 @@
 import java.util.*;
 
+class Solution {
+    int missingNumber(int array[], int n) {
+        int count =1;
+        Arrays.sort(array);
+        for(int i=0;i<array.length;i++){
+            if(array[i]==count){
+                count++;
+            }
+            else{
+                return count;
+            }
+        }
+        return count;
+    }
+}
 class practice{
+
     public static void main(String[] args){
-        int result = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the N value");
-        int[] num = new int[5];
-        for(int i=0;i<num.length;i++){
-            num[i]=sc.nextInt();
-        }
-        for(int i=0;i<num.length;i++){
-            result=result+num[i];
-        }
+        int array[]={1,2,3,5};
+        Solution sol = new Solution();
+        int result = sol.missingNumber(array,5);
         System.out.println(result);
     }
 }
