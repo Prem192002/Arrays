c06b426067885b3981dd9dd953d4c653432437d4
Prem192002 <premsingh192002@gmail.com>
2024-03-13 22:54:47 +0530

    cleaned the code

--- practice.java
+++ practice.java
@@ -2,25 +2,24 @@ import java.util.*;
 
 class Solution {
     int missingNumber(int array[], int n) {
-        int count =1;
+        int count = 1;
         Arrays.sort(array);
-        for(int i=0;i<array.length;i++){
-            if(array[i]==count){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == count) {
                 count++;
-            }
-            else{
+            } else {
                 return count;
             }
         }
         return count;
     }
 }
-class practice{
 
-    public static void main(String[] args){
-        int array[]={1,2,3,5};
+class practice {
+    public static void main(String[] args) {
+        int array[] = { 1, 2, 3, 5 };
         Solution sol = new Solution();
-        int result = sol.missingNumber(array,5);
+        int result = sol.missingNumber(array, 5);
         System.out.println(result);
     }
 }
