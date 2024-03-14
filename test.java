8c4f1d3842b2feb1b7a5a1e86a372aada59dfde2
Prem192002 <premsingh192002@gmail.com>
2024-03-14 21:41:37 +0530

    changed the test array

--- test.java
+++ test.java
@@ -4,7 +4,7 @@ class test{
         int right =1;
         int sum = 0;
         int target = 12;
-        int[] num = {1,2,3,7,5};
+        int[] num = {1,2,3,7,5,9};
         while(left<num.length-1){
             sum=num[left]+num[right];
             right++;
