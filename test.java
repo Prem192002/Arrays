d6ea27261b46d18b07f67235085794e5e073cf2b
Prem192002 <premsingh192002@gmail.com>
2024-03-24 23:26:20 +0530

    second largest

--- test.java
+++ test.java
@@ -1,40 +1,33 @@
+import java.util.*;
 class test {
-
-    
-    // a: input array
-    // n: size of array
-    // Function to find equilibrium point in the array.
-    public static int equilibriumPoint(long arr[], int n) {
-
-        // Your code here
-        long[] arr1=new long[n];
-        arr1[0]=arr[0];
-        for(int i=1;i<arr.length;i++){
-            arr1[i]=arr1[i-1]+arr[i];
-        }
-
-        long[] arr2=new long[n];
-        arr2[n-1]=arr[n-1];
-        for(int i=n-2;i<arr.length;i--){
-            if(i>=0){
-            arr2[i]=arr2[i+1]+arr[i];
+    public static int print2largest(int arr[], int n) {
+        // code here
+        Arrays.sort(arr);
+        int right=n-1;
+        int result = 0;
+        while (right>=0){
+            if(right==0 && arr[right]==arr[right+1]){
+                return -1;
             }
-            else break;
-        }
-
-        for(int i=0;i<arr.length;i++){
-            if(arr1[i]==arr2[i]){
-                return i+1;
+            else if (arr[right-1]!=arr[right]){
+                result= arr[right-1];
+                break;
             }
+
+            else if(arr[right-1]==arr[right]){
+                right--;
+            }
+
+
+            
+            
         }
-        
-        return -1;
+        return result;
         
     }
-
     public static void main(String[] args){
-        long[] arr = {1};
-        int result = equilibriumPoint(arr, 1);
-        System.out.println(result);
+    int [] arr = {10,10,10};
+    int fin= print2largest(arr,3);
+    System.out.println(fin);
     }
 }
