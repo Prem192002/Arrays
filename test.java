35d863ee4cc2c37950809e6a902705f319cf5108
Prem192002 <premsingh192002@gmail.com>
2024-04-17 23:15:43 +0530

    list test

--- test.java
+++ test.java
@@ -1,33 +1,19 @@
-import java.util.*;
-class test {
-    public static int print2largest(int arr[], int n) {
-        // code here
-        Arrays.sort(arr);
-        int right=n-1;
-        int result = 0;
-        while (right>=0){
-            if(right==0 && arr[right]==arr[right+1]){
-                return -1;
-            }
-            else if (arr[right-1]!=arr[right]){
-                result= arr[right-1];
-                break;
-            }
-
-            else if(arr[right-1]==arr[right]){
-                right--;
-            }
-
-
-            
-            
-        }
-        return result;
-        
-    }
+import java.util.ArrayList;
+class test{
     public static void main(String[] args){
-    int [] arr = {10,10,10,1};
-    int fin= print2largest(arr,3);
-    System.out.println(fin);
+    ArrayList<Integer> l1 = new ArrayList<>();
+    l1.add(5);
+    l1.add(10);
+    l1.add(15);
+    l1.add(20);
+    l1.add(25);
+    l1.add(1,100); 
+
+        // for(int i=0;i<l1.size();i++){
+        // System.out.println(l1.get(i));
+        // }
+
+        System.out.println(l1);
     }
+    
 }
