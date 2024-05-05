7d88213b74421ceebf43145940499bf416949fca
Prem192002 <premsingh192002@gmail.com>
2024-05-05 22:55:41 +0530

    pattern printing

--- two_pointers.java
+++ two_pointers.java
@@ -1,26 +1,18 @@
-public class two_pointers {
+class two_pointers {
     public static void main(String[] args) {
-        int[] arr = {7, 2, 9, 4, 3, 5};
-        
-        int i=0;
-        int j=arr.length-1;
-        while(i<j) {
-            if (arr[i] > arr[j]) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                j--;
-            } else {
-                j --;
-                if(i==j){
-                    i++;
-                    j=arr.length-1;
-                }
-            }
-        }
+        int n = 4;
+        char[][] matrix = new char[n][n];
 
-        for (int k = 0; k < arr.length; k++) {
-            System.out.print(arr[k] + " ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    matrix[i][j] = '*';
+                } else {
+                    matrix[i][j] = ' ';
+                }
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
