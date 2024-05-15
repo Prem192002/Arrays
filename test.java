1ab8a90578c54f21473cb42f48da082b4f51297d
Prem192002 <premsingh192002@gmail.com>
2024-05-15 23:40:11 +0530

    used oops

--- test.java
+++ test.java
@@ -1,7 +1,16 @@
+class fun{
+    int sum_frn(int a,int b){
+        int ans = a+b;
+        return ans;
+    }
+}
+
 class test{
-    public static void main(String args[]){
-        for(int i=1;i<10;i++){
-            System.out.println(i);
-        }
+    public static void main(String[] args) {
+       
+            fun obj = new fun();
+            int ans = obj.sum_frn(5,3);
+            System.out.println(ans);
+        
     }
 }
