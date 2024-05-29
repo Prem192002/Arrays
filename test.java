92c51d89803d2aa79b0cd59f2810bbb7860d4f77
Prem192002 <premsingh192002@gmail.com>
2024-05-29 19:07:11 +0530

    digits count

--- test.java
+++ test.java
@@ -1,17 +1,17 @@
 class test {
-    static int digit_sum(int n) {
+    static int digit_count(int n) {
         if(n>=0 && n<=9){
             return n;
         }
-        int last_digit = n%10;
-        int reset_sum = digit_sum(n/10);
+        int last_count = 1;
+        int reset_count= digit_count(n/10);
 
-        int ans = reset_sum+last_digit;
+        int ans = reset_count+last_count;
         return ans;
     }
 
     public static void main(String[] args) {
-            System.out.println(digit_sum(1234));
+            System.out.println(digit_count(1234589));
         
     }
 }
