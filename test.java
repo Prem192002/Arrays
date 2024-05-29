728490546c66f6f240614e18ab34ada58250cdbe
Prem192002 <premsingh192002@gmail.com>
2024-05-29 19:30:39 +0530

    power calculation

--- test.java
+++ test.java
@@ -1,17 +1,17 @@
 class test {
-    static int digit_count(int n) {
-        if(n>=0 && n<=9){
-            return n;
+    static int power(int p, int q) {
+        if(q==0){
+            return 1;
         }
-        int last_count = 1;
-        int reset_count= digit_count(n/10);
+        int last_count = p;
+        int reset_count= power(p,q-1);
 
-        int ans = reset_count+last_count;
+        int ans = reset_count*last_count;
         return ans;
     }
 
     public static void main(String[] args) {
-            System.out.println(digit_count(1234589));
+            System.out.println(power(6,3));
         
     }
 }
