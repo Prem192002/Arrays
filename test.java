122b8c178b19758ab66393601723a00715b2d26a
Prem192002 <premsingh192002@gmail.com>
2024-05-31 02:40:08 +0530

    max number in the array using recursion

--- test.java
+++ test.java
@@ -1,14 +1,19 @@
 class test {
-    static void printarray(int[]num, int idx) {
-        if(idx==num.length){
-            return;
+    static int maxarray(int[]num, int idx) {
+        if(idx==num.length-1){
+            return num[idx];
         }
-        System.out.println(num[idx]);
-        printarray(num, idx+1);
+        int max = maxarray(num, idx+1);
+        if(max>num[idx]){
+            return max;
+        }
+        else{
+            return num[idx];
+        }
     }
 
     public static void main(String[] args) {
-        int[] num = {5,8,2,7};
-        printarray(num, 0);
+        int[] num = {5,8,2,7,12};
+        System.out.println(maxarray(num, 0));
     }
 }
