f58b2878154032a6ac80bc2e52be98daea2be135
Prem192002 <premsingh192002@gmail.com>
2024-05-31 02:57:44 +0530

    array sum using recursion

--- test.java
+++ test.java
@@ -1,19 +1,15 @@
 class test {
-    static int maxarray(int[]num, int idx) {
+    static int arraysum(int[]num, int idx) {
         if(idx==num.length-1){
             return num[idx];
         }
-        int max = maxarray(num, idx+1);
-        if(max>num[idx]){
-            return max;
-        }
-        else{
-            return num[idx];
-        }
+        int smallsum = arraysum(num, idx+1);
+        int res = num[idx]+smallsum;
+        return res;
     }
 
     public static void main(String[] args) {
         int[] num = {5,8,2,7,12};
-        System.out.println(maxarray(num, 0));
+        System.out.println(arraysum(num, 0));
     }
 }
