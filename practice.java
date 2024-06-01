454dfc2439ff091a9633e6a6e505b79fcc4a9d22
Prem192002 <premsingh192002@gmail.com>
2024-06-02 02:36:26 +0530

    leaders using recursion

--- practice.java
+++ practice.java
@@ -1,32 +1,45 @@
 import java.util.*;
 
 class practice {
-    public static ArrayList<Integer> leaders(int arr[], int n, int idx){
-        
-        ArrayList<Integer> result = new ArrayList<>();
-        //base case
-        if(idx==n-1){
-        result.add(arr[n-1]);
-        return result;
-        }
-        //self work
-
-        if(arr[idx]>arr[idx+1] && arr[idx]>arr[idx-1]){
-            result.add(arr[idx]);
-        }
-        //regresion work
-        result.addAll(leaders(arr, n, idx+1));
-
-        return result;
+    // Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n) {
+        return leader(arr, n, 0);
     }
     
-
-    public static void main(String[] args) {
-        int arr[] = {16,17,4,3,5,2};
-        int n = arr.length;
-        for (int i = 0; i < leaders(arr,n,0).size(); i++) {
-            System.out.println(leaders(arr,n,0).get(i));
+    private static ArrayList<Integer> leader(int arr[], int n, int idx) {
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        // Base case
+        if (idx == n - 1) {
+            result.add(arr[n - 1]);
+            return result;
         }
-       
+        
+        //recursion work
+        ArrayList<Integer> restLeaders = new ArrayList<>();
+        restLeaders.addAll(leader(arr, n, idx + 1));
+        
+        // self work
+        if (arr[idx] >= restLeaders.get(0)) {
+            result.add(arr[idx]);
+        }
+        
+        // Add the rest of the leaders to the result
+        result.addAll(restLeaders);
+        
+        return result;
+    }
+    public static void main(String[] args) {
+        int arr[] = {16, 17, 4, 3, 5, 2};
+        int n = arr.length;
+
+        ArrayList<Integer> leaders = practice.leaders(arr, n);
+        for (int leader : leaders) {
+            System.out.println(leader);
+        }
     }
 }
+
+    
+
+    
