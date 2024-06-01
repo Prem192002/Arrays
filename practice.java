43fcdf0288aadc320e3c7cb3b0538e4922e4df2e
Prem192002 <premsingh192002@gmail.com>
2024-06-02 01:50:55 +0530

    leaders in an array using recursion

--- practice.java
+++ practice.java
@@ -1,37 +1,31 @@
 import java.util.*;
 
 class practice {
-    public static ArrayList<Integer> duplicates(int arr[], int n){
+    public static ArrayList<Integer> leaders(int arr[], int n, int idx){
         
         ArrayList<Integer> result = new ArrayList<>();
-        Arrays.sort(arr);
-        int left = 0;
-        int right = left+1;
-        while(right<arr.length){
-            if(arr[left]!=arr[right]){
-                right++;
-                left ++;
-            }
-            else if(arr[left]==arr[right]){
-                if(!result.contains(arr[right])){
-                result.add(arr[right]);
-                }
-                right++;
-                left++;
-            }
+        //base case
+        if(idx==n-1){
+        result.add(arr[n-1]);
+        return result;
         }
-        if(result.size()==0){
-            result.add(-1);
+        //self work
+
+        if(arr[idx]>arr[idx+1] && arr[idx]>arr[idx-1]){
+            result.add(arr[idx]);
         }
+        //regresion work
+        result.addAll(leaders(arr, n, idx+1));
+
         return result;
     }
     
 
     public static void main(String[] args) {
-        int arr[] = {3, 4, 12, 3, 12, 3, 4, 4, 12, 7, 11, 6, 5,4};
+        int arr[] = {16,17,4,3,5,2};
         int n = arr.length;
-        for (int i = 0; i < duplicates(arr,n).size(); i++) {
-            System.out.println(duplicates(arr,n).get(i));
+        for (int i = 0; i < leaders(arr,n,0).size(); i++) {
+            System.out.println(leaders(arr,n,0).get(i));
         }
        
     }
