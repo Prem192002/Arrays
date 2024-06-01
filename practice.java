14f2c842002f22062e0aa763947e210a50857a7d
Prem192002 <premsingh192002@gmail.com>
2024-06-02 01:18:12 +0530

    duplicates in an array

--- practice.java
+++ practice.java
@@ -1,25 +1,38 @@
 import java.util.*;
 
-class Solution {
-    int missingNumber(int array[], int n) {
-        int count = 1;
-        Arrays.sort(array);
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == count) {
-                count++;
-            } else {
-                return count;
+class practice {
+    public static ArrayList<Integer> duplicates(int arr[], int n){
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        Arrays.sort(arr);
+        int left = 0;
+        int right = left+1;
+        while(right<arr.length){
+            if(arr[left]!=arr[right]){
+                right++;
+                left ++;
+            }
+            else if(arr[left]==arr[right]){
+                if(!result.contains(arr[right])){
+                result.add(arr[right]);
+                }
+                right++;
+                left++;
             }
         }
-        return count;
+        if(result.size()==0){
+            result.add(-1);
+        }
+        return result;
     }
-}
+    
 
-class practice {
     public static void main(String[] args) {
-        int array[] = { 1, 2, 3, 5, 10,4,9};
-        Solution sol = new Solution();
-        int result = sol.missingNumber(array, 5);
-        System.out.println(result);
+        int arr[] = {3, 4, 12, 3, 12, 3, 4, 4, 12, 7, 11, 6, 5,4};
+        int n = arr.length;
+        for (int i = 0; i < duplicates(arr,n).size(); i++) {
+            System.out.println(duplicates(arr,n).get(i));
+        }
+       
     }
 }
