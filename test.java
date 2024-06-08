42ae715e80beeabf6a4307c5e11b5b825c64f8cd
Prem192002 <premsingh192002@gmail.com>
2024-06-09 03:30:08 +0530

    test cases

--- test.java
+++ test.java
@@ -1,37 +1,46 @@
 import java.util.*;
 class test{
     // Function to count the frequency of all elements from 1 to N in the array.
-    public static void frequencyCount(int arr[], int N, int P) {
-        // Step 1: Normalize elements greater than N to 0
-        int[] num = new int[P+2];
-        Arrays.sort(arr);
-        if(N>=P){
-        for(int i=0;i<N;i++){
-            num[arr[i]]++;
-        }
-        
-        }
-        else if(N<P){
-            for(int i=0;i<N;i++){
-                num[arr[i]]++;
+    
+    public static int  maxMeetings(int start[], int end[], int n)
+    {
+
+        // int [] start = {1,3,0,5,8,5};
+        // int [] end =  {2,4,6,7,9,9};
+        // sorting the arrays based on end time;
+
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(end[j]>end[j+1]){
+                    //swap
+                    int temp1=end[j];
+                    end[j]=end[j+1];
+                    end[j+1]=temp1;
+
+                    int temp2=start[j];
+                    start[j]=start[j+1];
+                    start[j+1]=temp2;
+                }
             }
         }
 
-        
-        
-        for (int i = 0; i < N; i++) {
-            arr[i] = num[i + 1];
+        int prev_time=end[0];
+        int count=0;
+        for(int i=1;i<n;i++){
+            if(start[i]>prev_time){
+                count++;
+                prev_time=end[i];
+            }
         }
+        return count+1;
+        
     }
-
     public static void main(String[] args) {
-        int[] arr1 = {2, 3, 2, 3, 5};
-        int N1 = arr1.length;
-        int P1 = 5;
-        frequencyCount(arr1, N1, P1);
-        for (int i = 0; i < N1; i++) {
-            System.out.print(arr1[i] + " ");
-        }
+        int[] start = {75250 ,50074, 43659, 8931, 11273, 27545, 50879, 77924};
+        int [] end = {112960, 114515, 81825, 93424, 54316, 35533, 73383, 160252};
+        int n= start.length;
+       int res=  maxMeetings(start,end,n);
+        System.out.println(res);
         
     }
 }
