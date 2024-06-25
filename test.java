77c7d64ad484e70dcbbf60a8f77728f1a08d94ae
Prem192002 <premsingh192002@gmail.com>
2024-06-25 23:45:50 +0530

    gfg changes

--- test.java
+++ test.java
@@ -1,39 +1,48 @@
 import java.util.*;
 
 class test {
-    public static ArrayList<Integer> countEleLessThanOrEqual(int arr1[], int arr2[]) {
-        ArrayList<Integer> result = new ArrayList<>();
+    static int lastIndex(int arr[], int st, int end,int target)
+    {
+        // {1,3,5,5,67,123,125}
 
-        Arrays.sort(arr2); // Sort arr2 for binary search
+        //code to find the last index
+        int mid= (st+end)/2;
+        int lastindex = arr.length-1;
+        while(st<=end){
+            
+            if(arr[mid]==target){
+                if(arr[mid+1]<=target){
+                    lastindex=mid;
+                    st=mid+1;
+                    mid=(st+end)/2;
+                }
 
-        for (int i = 0; i < arr1.length; i++) {
-            int target = arr1[i];
-            int left = 0;
-            int right = arr2.length - 1;
-            int count = 0;
-
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-
-                if (arr2[mid] <= target) {
-                    count = mid + 1;
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
+                if(arr[mid]>target){
+                    end=mid-1;
+                    mid=(st+end)/2;
                 }
             }
 
-            result.add(count);
+            if(arr[mid]>target){
+                end=mid-1;
+            }
+
+            if(arr[mid]<target){
+                st=mid+1;
+            }
         }
 
-        return result;
+        return lastindex+1;
+        
+
     }
+    
 
     public static void main(String[] args) {
-        int arr1[] = {4, 8, 7, 5, 1};
-        int arr2[] = {0, 1, 1, 3, 4, 5, 48};
-
-        ArrayList<Integer> counts = countEleLessThanOrEqual(arr1, arr2);
-        System.out.println("Counts: " + counts); // Output the counts
+        int arr1[] = {1,3,5,5,67,123,125};
+        
+        int res = lastIndex(arr1, 0, 6, 5);
+        System.out.println(res);
+   
     }
 }
