a12d3bcad71aa8247c4a4ab5ff76d6761a8d1ee0
Prem192002 <premsingh192002@gmail.com>
2024-07-22 01:27:55 +0530

    test commif

--- test.java
+++ test.java
@@ -1,26 +1,20 @@
 import java.util.*;
 
 class test {
-    static int lastIndex(int arr[], int st, int end,int target)
+    static int lastIndex(int arr[], int target)
     {
         // {1,3,5,5,67,123,125}
 
         //code to find the last index
-        int mid= (st+end)/2;
-        int lastindex = arr.length-1;
-        while(st<=end){
-            
-            if(arr[mid]==target){
-                if(arr[mid+1]<=target){
-                    lastindex=mid;
-                    st=mid+1;
-                    mid=(st+end)/2;
-                }
 
-                if(arr[mid]>target){
-                    end=mid-1;
-                    mid=(st+end)/2;
-                }
+        int st=0;
+        int end=arr.length-1;
+        int res=-1;
+        while(st<=end){
+            int mid=(st+end)/2;
+            if(arr[mid]==target){
+                res=mid;
+                st=mid+1;
             }
 
             if(arr[mid]>target){
@@ -32,7 +26,7 @@ class test {
             }
         }
 
-        return lastindex+1;
+        return res;
         
 
     }
@@ -41,7 +35,7 @@ class test {
     public static void main(String[] args) {
         int arr1[] = {1,3,5,5,67,123,125};
         
-        int res = lastIndex(arr1, 0, 6, 5);
+        int res = lastIndex(arr1, 5);
         System.out.println(res);
    
     }
